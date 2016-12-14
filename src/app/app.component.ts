7cb8ecac0a30e7caa0c077fad34397dfca2a859b
Daniel Nagy <Daniel Nagy>
2016-12-14 03:02:36 +0100

    lecture 3 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Article } from './article/article.model';
 
 @Component({
@@ -6,8 +6,35 @@ import { Article } from './article/article.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  onSubmit(title, link) {
-    console.log(title.value, link.value);
+export class AppComponent implements OnInit {
+
+  article: Article;
+  articles = [];
+
+  constructor() {
   }
+
+  ngOnInit() {
+    this.articles = [
+      new Article ('Angular 1', 'https://angular-ui.github.io/bootstrap/', 1),
+      new Article ('Angular 2', 'https://angular-ui.github.io/bootstrap/', 2),
+      new Article ('Angular 3', 'https://angular-ui.github.io/bootstrap/', 3),
+    ];
+    this.articles.sort(function(a, b) {return b.votes - a.votes; });
+  }
+
+  onVoteUp(evt) {
+    // this.article.voteUp();
+    console.log(evt);
+  }
+
+  onVoteDown() {
+    //
+  }
+
+  onSubmit(newArticle) {
+    newArticle = new Article (newArticle.title, newArticle.link);
+    this.articles.push(newArticle);
+  }
+
 }
