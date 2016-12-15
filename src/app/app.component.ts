bef6001ff33723a436ee3e0314b82196fe37b707
Daniel Nagy <Daniel Nagy>
2016-12-15 21:19:48 +0100

    Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Article } from './article/article.model';
-import { FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent implements OnInit {
 
-  article: Article;
-  articles = [];
-  title;
-  link;
-  articleForm;
-
-  constructor() {
-  }
+  constructor() { }
 
   ngOnInit() {
-
-    this.articleForm = new FormGroup({
-      title: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
-      link: new FormControl('', [Validators.required,
-        Validators.pattern('^(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$')])
-    });
-
-    this.articles = [
-      new Article ('Angular 1', 'https://angular-ui.github.io/bootstrap/', 1),
-      new Article ('Angular 2', 'https://angular-ui.github.io/bootstrap/', 2),
-      new Article ('Angular 3', 'https://angular-ui.github.io/bootstrap/', 3),
-    ];
-
-    this.articleForm.valueChanges.subscribe(value => {
-      console.log(value);
-
-    });
-
   }
-
-  onVoteUp(article) {
-    article.voteUp();
-  }
-
-  onVoteDown(article) {
-    article.voteDown();
-  }
-
-  onSubmit(newArticle) {
-    this.articles.push(new Article (newArticle.title, newArticle.link));
-    console.log(this.articles);
-  }
-
-  sortArticles() {
-    return this.articles.sort(function(a, b) { return b.votes - a.votes; });
-  }
-
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RedditComponent } from './reddit/reddit.component';
 import { ArticleComponent } from './article/article.component';
+import { ToDoListComponent } from './todolist/todolist.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+
+  { path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'reddit' },
+      { path: 'reddit', component: RedditComponent},
+      { path: 'todolist', component: ToDoListComponent},
+  ]},
+  { path: 'login', component: LoginComponent},
+  { path: '**', component: PageNotFoundComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    RedditComponent,
+    ArticleComponent,
+    ToDoListComponent,
+    PageNotFoundComponent,
+    LayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
